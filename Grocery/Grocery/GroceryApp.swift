9d32cdd28946f3a0fb486bec9cec4f4ef8cc611f
jb <jbdubillard@hotmail.com>
2024-02-27 14:36:47 +0100

    Project initialization

---
+++ Grocery/Grocery/GroceryApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryApp.swift
+//  Grocery
+//
+//  Created by Jean-baptiste DUBILLARD on 27/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct GroceryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
---
+++ Grocery/Grocery/Views/HomeView/HomeView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Grocery
+//
+//  Created by Jean-baptiste DUBILLARD on 27/02/2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    HomeView()
+}
